8630406565f4171329665ce69f6c50b1f082dfc9
jmbreda <jeremiebreda@disroot.org>
2025-01-29 15:46:40 +0100

    update gitignore (haas)

--- Cedric_transcriptome_mapping_pipeline/running_command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-snakemake  -s Snakefile  -j 80 --restart-times 2 --cluster-config cluster.json --ri --cluster "sbatch --cpus-per-task {cluster.n}  --time {cluster.time} --mem {cluster.mem} --qos serial"
-#snakemake  -s Snakefile  -j 999 --restart-times 3 --cluster-config cluster.json --use-envmodules --cluster "sbatch --cpus-per-task {cluster.n}  --time {cluster.time} --mem {cluster.mem} --qos serial"
-
-
